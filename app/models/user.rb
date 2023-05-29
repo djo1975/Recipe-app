3783ca09fe0ee2f1cfad9175cc5c6c9812e013a3
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-29 19:28:37 +0200

    Update User and Food models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :recipes, foreign_key: :author_id
+  has_many :recipes, foreign_key: :author_id, dependent: :destroy
+  has_many :foods, foreign_key: :author_id, dependent: :destroy
 end
