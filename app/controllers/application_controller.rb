5c8fc85216be357f7b0d32f75eea201d980e434a
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-30 12:17:32 +0200

    Adapt remote changes to local env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    # Dummy korisnik ili privremeni objekt
-    User.new(name: 'Test User')
+    User.last
   end
 end
