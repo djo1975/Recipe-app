471a98649e3a8608c9b0c9afd88effa13b197323
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-31 08:10:31 +0200

    Add validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,9 @@ class Food < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :food_recipes, dependent: :destroy
   has_many :recipes, through: :food_recipes
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :measurement_unit, presence: true, length: { minimum: 2, maximum: 10 }
+  validates :price, presence: true, numericality: { decimal: true, greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :prep_time, :cook_time, presence: true, length: { minimum: 5, maximum: 15 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :public, inclusion: { in: [true, false] }
 end
