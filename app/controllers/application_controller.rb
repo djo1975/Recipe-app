99069b2a1983c02a10b64d9ed2057e39694844da
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-05-31 20:01:44 +0200

    Generate cancan abilty.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 end
