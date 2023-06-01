da37843eeb2fcd6bb5dc6252c4385dde768d4bc0
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-06-01 17:29:25 +0200

    Update RecipeFoods migration

--- db/migrate/20230529133842_create_recipe_foods.rb
+++ db/migrate/20230529133842_create_recipe_foods.rb
@@ -3,6 +3,7 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
     create_table :recipe_foods do |t|
       t.references :recipe, null: false, foreign_key: true
       t.references :food, null: false, foreign_key: true
+      t.integer :quantity, null: false
 
       t.timestamps
     end
