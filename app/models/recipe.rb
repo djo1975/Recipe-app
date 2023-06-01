49a445b39daab90f59447d27c45d9d5f37394c69
djo1975 <mladanilic1975@gmail.com>
2023-06-01 10:14:13 +0200

    Branch is ready

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  # has_many :recipe_foods
+  # has_many :foods, through: :recipe_foods
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :prep_time, :cook_time, presence: true, length: { minimum: 5, maximum: 15 }
